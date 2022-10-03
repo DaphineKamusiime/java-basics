164a0eb69b17483fe8e7a103b924de46ddc87b54
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-03 02:57:05 -0700

    Add files via upload

---
+++ Monday.java
@@ -0,0 +1,34 @@
+public class Monday{
+   public static void main(String[] args){
+       System.out.println("....................");
+       String name="Daphine";
+       System.out.println(name);
+       System.out.println("year="+2022);
+       System.out.print("8*9=");
+       System.out.println(8*9);s
+       String first_name="Daphine";
+       String second_name=" ssKamusiime"; 
+       String full_name=first_name+second_name;
+       System.out.println(full_name);
+       int x=3,y=7,z=9;
+       System.out.println(x*y*z);
+       float result=9.87f;
+       boolean mybool=false;
+       System.out.println(result);
+       System.out.println(mybool);
+       float f1=34e3f;
+       double d1=12E4d;
+       System.out.println(f1);
+       System.out.println(d1); 
+       //widening casting
+       int num=3;
+       double number=num;
+       System.out.println(num);
+       System.out.println(number);
+       //narrowing casting
+       double mynum=12.43d;
+       int mynumber=(int)mynum;
+       System.out.println(mynum);
+       System.out.println(mynumber);
+  }
+}
